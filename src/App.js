fc9ef65e4930edf19ba4456ef67b4beb03c06551
avi-technomads <avi.patel@technomads.in>
2022-09-15 11:40:09 +0530

    Almost done

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import Battle from './components/Battle/Battle';
-import { Route, Routes } from 'react-router';
+import {  Routes, Route } from 'react-router-dom';
+
 import Results from './components/Results/Results';
 
 const App = () => {
 	return (
-	<>
-		<Header />
-		<Routes>
-			<Route exact path='/' element={<Home />} />
-			<Route exact path='/battle' element={<Battle />} />
-			<Route exact path='/result' element={<Results />} />
-		</Routes>
-		{/* <Results /> */}
-		<Footer />
-	</>
-);
+		<>
+			<Header />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route exact path='/battle' element={<Battle />} />
+				<Route exact path='/result' element={<Results />} />
+			</Routes>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
